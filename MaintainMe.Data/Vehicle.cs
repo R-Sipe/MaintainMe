e5c99bf48a95cd17043190e26930eceef861714d
R-Sipe <raymondsipes21@gmail.com>
2021-07-21 12:33:50 -0400

    added vehicle data layer

--- MaintainMe.Data/Vehicle.cs
+++ MaintainMe.Data/Vehicle.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace MaintainMe.Data
 {
+    public enum VehicleType { Sedan=1, Hatchback, SUV, Truck, Coupe, Van}
     public class Vehicle
     {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public VehicleType Type { get; set; }
     }
 }
