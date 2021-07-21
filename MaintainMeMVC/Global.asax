803aefd6910c90cb2e8e9c6afe3f5fc5ebe6a3d7
R-Sipe <raymondsipes21@gmail.com>
2021-07-21 12:18:03 -0400

    Add project files.

---
+++ MaintainMeMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaintainMeMVC.MvcApplication" Language="C#" %>
---
+++ MaintainMeMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaintainMeMVC.Startup))]
+namespace MaintainMeMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
