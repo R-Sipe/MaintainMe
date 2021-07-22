e8f615da13aa4975582455e523d017e738bd0d37
R-Sipe <raymondsipes21@gmail.com>
2021-07-22 09:43:07 -0400

    added create and get from service for vehicle

--- MaintainMe.Services/VehicleService.cs
+++ MaintainMe.Services/VehicleService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MaintainMe.Data;
+using MaintainMe.Models.VehicleModel;
+using MaintainMeMVC.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,40 @@ namespace MaintainMe.Services
 {
     public class VehicleService
     {
+        public bool CreateVehicle(VehicleCreate create)
+        {
+            var entity =
+                new Vehicle()
+                {
+                    Make = create.Make,
+                    Model = create.Model,
+                    Year = create.Year,
+                    Type = create.Type
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Vehicles.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
 
+        public IEnumerable<VehicleListItem> GetVehicles()
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Vehicles
+                        .Select(
+                        e =>
+                        new VehicleListItem
+                        {
+                            Make = e.Make,
+                            Model = e.Model,
+                            Year = e.Year
+                        });
+                return query.ToArray(); 
+            }
+        }
     }
 }
