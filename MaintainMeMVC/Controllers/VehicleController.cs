9ef44e2a98f5d0afb729dbce38497fcec4de13d5
R-Sipe <raymondsipes21@gmail.com>
2021-07-22 09:32:13 -0400

    added a create, view, and controller for create, creating service

--- MaintainMeMVC/Controllers/VehicleController.cs
+++ MaintainMeMVC/Controllers/VehicleController.cs
@@ -16,5 +16,21 @@ namespace MaintainMeMVC.Controllers
             var model = new VehicleListItem[0];
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(VehicleCreate create)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(create);
+        }
     }
 }
