393f621c933daf645dccaea31a18b132c9b71222
R-Sipe <raymondsipes21@gmail.com>
2021-07-22 08:53:51 -0400

    added vehicle list item, cpntroller, and view

---
+++ MaintainMeMVC/Controllers/VehicleController.cs
@@ -0,0 +1,20 @@
+﻿using MaintainMe.Models.VehicleModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaintainMeMVC.Controllers
+{
+    [Authorize]
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            var model = new VehicleListItem[0];
+            return View(model);
+        }
+    }
+}
